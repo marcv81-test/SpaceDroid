e6a95f577e7c813502600dd0e2297a12cd6ab271
Marc <marc.venturini@gmail.com>
2014-08-08 21:35:17 +0200

    Fixed background flickers

--- src/marcv81/gfx2d/Sprite.java
+++ src/marcv81/gfx2d/Sprite.java
@@ -68,7 +68,7 @@ public class Sprite {
         textureName = names[0];
         gl.glBindTexture(GL10.GL_TEXTURE_2D, textureName);
         gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER,
-                GL10.GL_NEAREST);
+                GL10.GL_LINEAR);
         gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER,
                 GL10.GL_LINEAR);
         GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
